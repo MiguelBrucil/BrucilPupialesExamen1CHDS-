aaa3b2edc9b6e1720f249358305e048da0febe06
MiguelBrucil <jose.brucil060@ist17dejulio.edu.ec>
2024-12-09 09:41:41 -0500

    Permisos de internet al cliente

--- app/src/main/java/com/example/brucilpupialesexamen1chds/MainActivity.java
+++ app/src/main/java/com/example/brucilpupialesexamen1chds/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.brucilpupialesexamen1chds;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +12,54 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
 
+public class MainActivity extends AppCompatActivity {
+    private Button btnRequest;
+    private TextView txtVista, txtVista2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        btnRequest = findViewById(R.id.btnCli);
+        txtVista = findViewById(R.id.txt_Cli);
+        txtVista2 = findViewById(R.id.textView4);
+
+        btnRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                obtenerServicioWeb("http://192.168.10.105:3000");
+            }
+        });
+    }
+
+    private void obtenerServicioWeb(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        txtVista.setText(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("clave", "valor");
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
 
     }
 }

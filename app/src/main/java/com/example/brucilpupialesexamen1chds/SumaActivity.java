65ae1b4bba086b1cc024c1cdad9ba041bfdc3454
MiguelBrucil <jose.brucil060@ist17dejulio.edu.ec>
2024-12-16 16:12:12 -0500

    Suma con cosntante

--- app/src/main/java/com/example/brucilpupialesexamen1chds/SumaActivity.java
+++ app/src/main/java/com/example/brucilpupialesexamen1chds/SumaActivity.java
@@ -1,6 +1,10 @@
 package com.example.brucilpupialesexamen1chds;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,50 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SumaActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+public class SumaActivity extends AppCompatActivity {
+    Button btnRequest;
+    TextView txtVista, txtVista2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_suma);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnRequest = findViewById(R.id.btnCli);
+        txtVista = findViewById(R.id.txt_Cli);
+        txtVista2 = findViewById(R.id.textView4);
+
+        btnRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                obtenerServicioWeb("http://10.10.24.39:3002/suma");
+            }
         });
     }
+
+    private void obtenerServicioWeb(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL,
+
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        txtVista.setText(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+        Volley.newRequestQueue(this).add(stringRequest);
+    }
 }
+    

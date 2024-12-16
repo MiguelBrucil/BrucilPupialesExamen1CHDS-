bd219c5d03af0358c650e25542fcc7fc001cf29f
MiguelBrucil <jose.brucil060@ist17dejulio.edu.ec>
2024-12-16 14:40:13 -0500

    Menu principal deL consumo

--- app/src/main/java/com/example/brucilpupialesexamen1chds/MenuActivity.java
+++ app/src/main/java/com/example/brucilpupialesexamen1chds/MenuActivity.java
@@ -15,7 +15,7 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btnSuma2), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
